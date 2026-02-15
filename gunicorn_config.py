768b7d919bcd65c24bbb6dd8dd87a7399cb1d86a
Nixy212 <149589590+Nixy212@users.noreply.github.com>
2026-02-15 18:03:54 +0100

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,17 +1,8 @@
-# Configuration Gunicorn pour Render
-# worker_class threading = compatible Python 3.14, supporte WebSockets via Socket.IO
-
 workers = 1
 worker_class = 'gthread'
 threads = 4
 timeout = 120
 keepalive = 75
-
 accesslog = '-'
 errorlog = '-'
-loglevel = 'info'
-
-bind = '0.0.0.0:10000'
-
-max_requests = 1000
-max_requests_jitter = 50
+loglevel = 'debug'
